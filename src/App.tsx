e96b5684fcc009c33d0ac0c8257aa34b18fee94b
Francisco Maik <chiconunes3081@gmail.com>
2021-06-16 16:32:50 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container, TextApp } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <TextApp>App</TextApp>
+    </Container>
+  );
+};
+
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+`;
+
+export const TextApp = styled.Text`
+  font-size: 40px;
+  font-weight: 700;
+  color: #719200;
+`;
